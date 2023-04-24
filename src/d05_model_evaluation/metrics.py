d13f35e571219cdc6f911a1da7fb0c1b6efbc527
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-04-24 14:37:42 -0500

    feat(src/d05_model_evaluation/metrics.py): Initial commit

---
+++ src/d05_model_evaluation/metrics.py
@@ -0,0 +1,33 @@
+"""
+Evaluate model against test set, provide metrics
+"""
+
+from pyspark.mllib import RegressionMetrics
+from pyspark.ml.regression import RandomForestRegressor
+from pyspark.sql.types import DoubleType
+
+
+def create_predictions(test, trained_model):
+    predictions_df = trained_model.transform(test)
+    return predictions_df
+
+
+def calculate_metrics(predictions_df):
+    val_pred_df = predictions.select(["totalinsurancepremiumofthepolicy", "prediction"])
+    val_pred_df = val_pred_df.withColumn(
+        "totalinsurancepremiumofthepolicy",
+        val_pred_df["totalinsurancepremiumofthepolicy"].cast(DoubleType()),
+    )
+
+    val_pred = val_pred_df.rdd.map(tuple)
+
+    metrics = RegressionMetrics(val_pred)
+
+    metrics_dict = {}
+    metrics_dict["r2"] = metrics.r2
+    metrics_dict["explainedVariance"] = metrics.explainedVariance
+    metrics_dict["meanAbsoluteError"] = metrics.meanAbsoluteError
+    metrics_dict["meanSquaredError"] = metrics.meanSquaredError
+    metrics_dict["rootMeanSquaredError"] = metrics.rootMeanSquaredError
+
+    return metrics_dict
