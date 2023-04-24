900074af028b81ee901a216d8229c5c56be96499
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-04-24 13:48:34 -0500

    feat(src/__init__.py): Pass spark session to function

--- src/__init__.py
+++ src/__init__.py
@@ -3,9 +3,14 @@ Calls all other methods to clean data, train model, and assess accuracy
 
 """
 import os
+import pyspark
 
-from d01_data.to_parquet import clean_write_parquet
+from d01_data.to_parquet import clean_write_parquet, new
+
+sc = pyspark.SparkContext("local[*]")
+spark = pyspark.sql.SparkSession(sc)
+
 
 # Create parquet if it doesn't yet exist
 if not os.path.exists("../data/02_intermediate/nfip-flood-policies.parquet"):
-    clean_write_parquet()
+    clean_write_parquet(spark)
