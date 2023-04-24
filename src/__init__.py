d3e06895884a6396d219773cb3d205bd57082a43
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-04-24 14:37:24 -0500

    feat(src/__init__.py): Train model

--- src/__init__.py
+++ src/__init__.py
@@ -5,9 +5,12 @@ Calls all other methods to clean data, train model, and assess accuracy
 import os
 import pyspark
 
-from d01_data.to_parquet import clean_write_parquet
 from d01_data.read_parquet import clean_parquet_df
+from d02_intermediate.to_parquet import clean_write_parquet
+from d03_processing.input_model import transform_info_vector
+from d05_model_evaluatoin import create_predictions, calculate_metrics
 
+TRAIN_MODEL = True
 
 sc = pyspark.SparkContext("local[*]")
 spark = pyspark.sql.SparkSession(sc)
@@ -20,6 +23,9 @@ if not os.path.exists("../data/02_intermediate/nfip-flood-policies.parquet"):
 # Read parquet if it exists
 if os.path.exists("../data/02_intermediate/nfip-flood-policies.parquet"):
     df = clean_parquet_df(spark)
+
+if TRAIN_MODEL:
+    train_df, test_df, validation_df = transform_info_vector(spark, df)
 
 
 df.show()
