a326d2ea488291a4c0e7927aa01fee685c7448cd
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-04-24 14:07:33 -0500

    feat(src/d03_processing/input_model.py): Initial commit

---
+++ src/d03_processing/input_model.py
@@ -0,0 +1,38 @@
+"""
+Converts Dataframe into vectors for model, splits data into training, test, validation sets
+"""
+
+from pyspark.ml.linalg import Vectors
+from pyspark.ml.feature import VectorAssembler
+
+
+def transform_into_vector(spark, df):
+    """Transforms features from DataFrame into vector column, then returns train, test, validation split.
+
+    :param spark: Spark session from __init__.py
+    :type spark: pyspark.SparkSession
+    :param df: DataFrame of cleaned data
+    :type df: DataFrame
+    :returns: test, train, validation DataFrames (0.6/0.2/0.2 split)
+
+    """
+    assembler = VectorAssembler(
+        # Exclude string columns
+        inputCols=[
+            "censustract",
+            "crsdiscount",
+            "elevationdifference",
+            "federalpolicyfee",
+            "hfiaasurcharge",
+            "latitude",
+            "longitude",
+            "numberoffloorsininsuredbuilding",
+            "policycost",
+            "policycount",
+        ],
+        outputCol="features",
+    )
+    output = assembler.transform(df)
+    final_df = output.select(["features", "totalinsurancepremiumofthepolicy"])
+
+    return final_df.randomSplit([0.6, 0.2, 0.2], seed=0)
