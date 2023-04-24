7cd38afa5a82dfdd5500f20c9c22e6c6d8e60624
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-04-24 13:59:32 -0500

    feat(src/__init__.py): Reads parquet from file

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,9 @@ Calls all other methods to clean data, train model, and assess accuracy
 import os
 import pyspark
 
-from d01_data.to_parquet import clean_write_parquet, new
+from d01_data.to_parquet import clean_write_parquet
+from d01_data.read_parquet import clean_parquet_df
+
 
 sc = pyspark.SparkContext("local[*]")
 spark = pyspark.sql.SparkSession(sc)
@@ -14,3 +16,10 @@ spark = pyspark.sql.SparkSession(sc)
 # Create parquet if it doesn't yet exist
 if not os.path.exists("../data/02_intermediate/nfip-flood-policies.parquet"):
     clean_write_parquet(spark)
+
+# Read parquet if it exists
+if os.path.exists("../data/02_intermediate/nfip-flood-policies.parquet"):
+    df = clean_parquet_df(spark)
+
+
+df.show()
