1f316c25b0026d3904a2d9d92ed989738a6d1e2c
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-04-24 19:42:56 -0500

    feat(src/__init__.py): Checks if path is correct

--- src/__init__.py
+++ src/__init__.py
@@ -10,6 +10,15 @@ from d02_intermediate.to_parquet import clean_write_parquet
 from d03_processing.input_model import transform_into_vector
 from d05_model_evaluation.metrics import create_predictions, calculate_metrics
 
+
+# If not called from toplevel of project,
+if os.getcwd().split("/")[-1] != "forest-for-flood":
+    os.chdir("..")
+    if os.getcwd().split("/")[-1] != "forest-for-flood":
+        raise ValueError(
+            "Make sure to start program from toplevel (eg. python src/__init__.py)"
+        )
+
 TRAIN_MODEL = True
 
 sc = pyspark.SparkContext("local[*]")
@@ -17,11 +26,11 @@ spark = pyspark.sql.SparkSession(sc)
 
 
 # Create parquet if it doesn't yet exist
-if not os.path.exists("../data/02_intermediate/nfip-flood-policies.parquet"):
+if not os.path.exists("data/02_intermediate/nfip-flood-policies.parquet"):
     clean_write_parquet(spark)
 
 # Read parquet if it exists
-if os.path.exists("../data/02_intermediate/nfip-flood-policies.parquet"):
+if os.path.exists("data/02_intermediate/nfip-flood-policies.parquet"):
     df = clean_parquet_df(spark)
 
 if TRAIN_MODEL:
