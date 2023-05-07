87ba0ceda0a5ef2474415b2fd047d38177d5c8ab
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-05-06 19:12:10 -0500

    feat(src/d04_modelling/train_model.py): Add number of trees in forest, make logs more verbose

--- src/d04_modelling/train_model.py
+++ src/d04_modelling/train_model.py
@@ -6,42 +6,47 @@ import logging
 from pyspark.ml.regression import RandomForestRegressor
 
 
-def create_model(depth):
+def create_model(depth, num_trees):
     """Creates model, sets label column
 
     :param depth: Set maximum depth of trees in forest
     :type depth: int
+    :param num_trees: Set number of trees in forest
+    :type num_trees: int
     :returns: model object
 
     """
     model = RandomForestRegressor(
-        labelCol="totalinsurancepremiumofthepolicy", maxDepth=depth, numTrees=40
+        labelCol="totalinsurancepremiumofthepolicy", maxDepth=depth, numTrees=num_trees
     )
     model.setSeed(0)
 
     return model
 
 
-def fit_model(train_df, depth, save=False):
+def fit_model(train_df, depth, num_trees, save=False):
     """Fits model to training data
 
     :param train_df: Training data
     :type train_df: DataFrame
     :param depth: Set maximum depth of trees in forest
     :type depth: int
+    :param num_trees: Set number of trees in forest
+    :type num_trees: int
     :param save: Set to save model to file if True
     :type save: bool
     :returns: RandomForestRegressor object of trained model
 
     """
-    model = create_model(depth)
-    logging.info("Started fitting model")
+    model = create_model(depth, num_trees)
+    logging.info(f"Started fitting model {depth}, {num_trees}")
     trained_model = model.fit(train_df)
 
-    model_name = f"data/04_models/model_depth_{depth}"
-
     if save:
+        model_name = f"data/04_models/model_{depth}_{num_trees}"
         trained_model.save(model_name)
-        logging.info(f"Successfully trained model with depth {depth}")
+        logging.info(
+            f"Successfully trained model with depth {depth} and {num_trees} trees"
+        )
 
     return trained_model
